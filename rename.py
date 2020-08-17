207c0120368c6607418970874ecd094a35c70a98
hyeyoomj <37920278+hyeyoomj@users.noreply.github.com>
2020-08-17 21:45:11 +0900

    renaming file upload

---
+++ rename.py
@@ -0,0 +1,10 @@
+import os
+
+def rename(path, name):
+    idx = 0
+    for filename in os.listdir(path):
+        print(path + filename, '=>', path + str(name) + '_' + str(idx) + '.jpeg')
+        os.rename(path + filename, path + str(name) + '_' + str(idx) + '.jpeg')
+        idx += 1
+
+rename('./data/test/gajeon/', 'gajeon')
