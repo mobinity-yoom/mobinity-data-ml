4f12fd09e57f260ce290c017d26d7b63b76d7ac6
hyeyoomj <21700680@handong.edu>
2020-08-27 18:22:39 +0900

    .

--- rename.py
+++ rename.py
@@ -4,7 +4,7 @@ def rename(path, name):
     idx = 0
     for filename in os.listdir(path):
         print(path + filename, '=>', path + str(name) + '_' + str(idx) + '.jpeg')
-        os.rename(path + filename, path + str(name) + '_' + str(idx) + '.jpg')
+        os.rename(path + filename, path + str(name) + '_' + str(idx) + '.jpeg')
         idx += 1
 
-rename('./instrument/', 'instrument')
+rename('./labeling/sports/', 'sports')
